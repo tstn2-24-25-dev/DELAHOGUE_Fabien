0e1293b580bdc02f3cbd6a706266e104c52b4a9d
D-Fabien <fabien.delahogue@gmail.com>
2024-09-11 11:54:27 +0200

    exo inventaire

--- Inventaire/inventaire.py
+++ Inventaire/inventaire.py
@@ -1,8 +1,27 @@
 
-def inventaire(chemin_fichier):
+def lire_data(chemin_fichier):
     with open(chemin_fichier) as f:
-        print(f.readline())
-    return f.readline
+
+        nb_article = f.readline().strip()
+        liste_article = f.readlines()
+        liste_complete = [nb_article]
+        for line in liste_article :
+            art = line.strip()
+            liste_complete.append(art)
+        return (liste_complete)
 
 
-inventaire('DELAHOGUE_Fabien/DELAHOGUE_Fabien/Inventaire/article.txt')
+
+
+def affichage(liste_classee):
+    for tup in liste_classee:
+        print(tup)
+
+
+def inventaire(chemin_fichier):
+    liste_classee = lire_data(chemin_fichier)
+    print(liste_classee)
+    affichage(liste_classee)
+
+
+print(inventaire('Inventaire/article.txt'))
